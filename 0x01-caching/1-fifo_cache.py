b09c5aef89312b0df3567a8f1c62d0225e9896e0
Programmingwithjerry <eneyellowj@gmail.com>
2024-10-31 13:25:14 +0100

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -8,8 +8,9 @@ from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """A cache that stores items with a First-In First-Out (FIFO) eviction policy.
-    Items are removed in the order they were added when the cache limit is reached.
+    """A cache that stores items with a First-In First-Out
+      (FIFO) eviction policy. Items are removed in the order
+      they were added when the cache limit is reached.
     """
 
     def __init__(self):
@@ -19,12 +20,14 @@ class FIFOCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Stores an item in the cache. Removes the oldest item if the cache is full.
+        """Stores an item in the cache. Removes the oldest
+           item if the cache is full.
         """
         if key and item:
             self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            first_key, _ = self.cache_data.popitem(False)  # Remove the first-added item
+            # Remove the first-added item
+            first_key, _ = self.cache_data.popitem(False)
             print("DISCARD:", first_key)
 
     def get(self, key):
