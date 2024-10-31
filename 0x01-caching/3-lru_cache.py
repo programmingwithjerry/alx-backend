f539ed9f7861eeab0602ca279d4714a276275ece
Programmingwithjerry <eneyellowj@gmail.com>
2024-10-31 12:53:13 +0100

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -20,21 +20,25 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Stores an item in the cache. If the cache is full, removes the least recently used item.
+        """Stores an item in the cache. If the cache is full, removes the
+           least recently used item.
         """
         if key is None or item is None:
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)  # Discard the least recently used item
+                # Discard the least recently used item
+                lru_key, _ = self.cache_data.popitem(True)
                 print("DISCARD:", lru_key)
             self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)  # Place the new item at the start
+            # Place the new item at the start
+            self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Returns the item associated with the specified key and marks it as recently used.
+        """Returns the item associated with the specified key
+           and marks it as recently used.
         """
         if key is not None and key in self.cache_data:
             self.cache_data.move_to_end(key, last=False)  # Mark as recently used
