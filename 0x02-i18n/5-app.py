3756c5031a865c98c95d35f70c11d78d73e6f3c1
programmingwithjerry <eneyellowj@gmail.com>
2024-11-05 22:45:37 +0100

    5-app.py

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+"""
+Basic Flask application setup.
+"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """
+    Application configuration class, defining supported languages,
+    and default locale and timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# Apply the configuration settings to the app
+app.config.from_object(Config)
+
+# Sample user data with information on name, locale, and timezone
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(login_as):
+    """
+    Retrieves user information based on the provided login ID.
+    """
+    try:
+        return users.get(int(login_as))
+    except Exception:
+        return
+
+
+@app.before_request
+def before_request():
+    """
+    Runs before handling each request to set the current user
+    in the global context if specified.
+    """
+    g.user = get_user(request.args.get("login_as"))
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determines the best language match for the current request.
+    Checks for a 'locale' parameter in the request and returns it
+    if valid, or falls back to the 'Accept-Language' header.
+    """
+    locale = request.args.get("locale")
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=["GET"], strict_slashes=False)
+def hello():
+    """
+    Renders the main template.
+    """
+    return render_template('5-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
